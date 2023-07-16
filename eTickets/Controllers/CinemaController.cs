2702637315a4b3d0ac9f7f04537b624b127f515f
Tony <tianbos@student.unimelb.edu.au>
2023-07-17 01:34:13 +1000

    add functionality to delete cinemas

--- eTickets/Controllers/CinemaController.cs
+++ eTickets/Controllers/CinemaController.cs
@@ -69,5 +69,29 @@ namespace eTickets.Controllers
             await _service.UpdateAsync(id, cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: Cinema/Delete/id
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cinemaDetails = await (_service.GetByIdAsync(id));
+            if (cinemaDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
