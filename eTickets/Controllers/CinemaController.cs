65bb42db34d156ca670b316e4bcbb2b87a023a65
Tony <tianbos@student.unimelb.edu.au>
2023-07-17 01:28:31 +1000

    add functionality to udpate cinemas

--- eTickets/Controllers/CinemaController.cs
+++ eTickets/Controllers/CinemaController.cs
@@ -47,5 +47,27 @@ namespace eTickets.Controllers
             }
             return View(cinemaDetails);
         }
+
+        //Get: Cinema/Edit/id
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(cinemaDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Logo, Name, Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cinema);
+            }
+            await _service.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
