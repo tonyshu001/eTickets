7c0a6e2807c70fcd0ec9c1f55f236e70721cbca9
Tony <tianbos@student.unimelb.edu.au>
2023-07-24 23:33:52 +1000

    add order and orderitem models

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -36,6 +36,10 @@ namespace eTickets.Data
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
 
+        //Orders related table
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
 
     }
 }
