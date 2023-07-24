66df7feb1eb8d5cbed94532ea25ab2cfabc04bdd
Tony <tianbos@student.unimelb.edu.au>
2023-07-25 00:19:27 +1000

    add order controller

---
+++ eTickets/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IMovieService _movieService;
+        private readonly ShoppingCart _shoppingCart;
+        public OrderController(IMovieService movieService, ShoppingCart shoppingCart)
+        {
+            _movieService = movieService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var responce = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal(),
+            };
+            return View(responce);
+        }
+    }
+}
