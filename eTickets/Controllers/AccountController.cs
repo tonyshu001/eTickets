1bcfd9e6b225d09bc83ee138d45342b6e31776e1
Tony <tianbos@student.unimelb.edu.au>
2023-08-05 03:39:41 +1000

    add functionality for users to log in

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -24,5 +24,35 @@ namespace eTickets.Controllers
             var response = new LoginVM();
             return View(response);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login (LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movie");
+                    }
+                }
+
+                TempData["Error"] = "Wrong credentials. Please try again";
+                return View(loginVM);
+
+            }
+
+            TempData["Error"] = "Wrong credentials. Please try again";
+            return View(loginVM);
+        }
     }
 }
