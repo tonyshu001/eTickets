8c9ddace51fe59a7f282cfedbc5b89d0ff008270
Tony <tianbos@student.unimelb.edu.au>
2023-08-06 00:12:13 +1000

    add functionality to log out users

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -93,5 +93,12 @@ namespace eTickets.Controllers
 
             return View("RegisterCompleted");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movie");
+        }
     }
 }
