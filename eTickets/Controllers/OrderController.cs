ff4559f46fe13e843f2f1518fdf9e70dc30cb0dc
Tony <tianbos@student.unimelb.edu.au>
2023-08-06 22:11:36 +1000

    add role based order view

--- eTickets/Controllers/OrderController.cs
+++ eTickets/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using eTickets.Data.Services;
 using eTickets.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace eTickets.Controllers
 {
@@ -19,8 +20,9 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            string userId = "";
-            var orders = await _orderService.GetOrderByUserIdAsync(userId);
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = User.FindFirstValue(ClaimTypes.Role);
+            var orders = await _orderService.GetOrderByUserIdAndRoleAsync(userId, userRole);
             return View(orders);
         }
 
@@ -65,8 +67,8 @@ namespace eTickets.Controllers
         public async Task<IActionResult> CompleteOrder()
         {
             var items = _shoppingCart.GetShoppingCartItems();
-            string userId = "";
-            string userEmailAddress = "";
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
 
             await _orderService.StoreOrderAsync(items, userId, userEmailAddress);
             await _shoppingCart.ClearShoppingCartAsync();
--- eTickets/Data/Services/OrderService.cs
+++ eTickets/Data/Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Static;
+using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data.Services
@@ -12,13 +13,18 @@ namespace eTickets.Data.Services
             _context = context;
         }
 
-        public async Task<List<Order>> GetOrderByUserIdAsync(string userId)
+        public async Task<List<Order>> GetOrderByUserIdAndRoleAsync(string userId, string userRole)
         {
             var orders = await _context.Orders
                 .Include(n => n.OrderItems)
                 .ThenInclude(n => n.Movie)
-                .Where(n => n.UserId == userId)
+                .Include(n => n.User)
                 .ToListAsync();
+
+            if(userRole != UserRoles.Admin) 
+            {
+                orders = orders.Where(n => userId == n.UserId).ToList();
+            }
             return orders;
         }
 
